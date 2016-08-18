210819e576d51210bc34f713d1ecb44dddc11a97
ndonohoe-bjss <nicholas.donohoe@bjss.com>
2016-08-18 09:57:37 +0100

    GMC-185 unit test for auto identity

--- mobile-connect-sdk/GSMA.MobileConnect.Test/EndToEndTests.cs
+++ mobile-connect-sdk/GSMA.MobileConnect.Test/EndToEndTests.cs
@@ -54,6 +54,7 @@ namespace GSMA.MobileConnect.Test
 
             Assert.AreEqual(MobileConnectResponseType.Complete, status.ResponseType);
             Assert.IsNotEmpty(status.TokenResponse.ResponseData.AccessToken);
+            Assert.IsNotEmpty(status.TokenResponse.ResponseData.IdToken);
         }
 
         [TestCase("SandboxR2")]
@@ -96,6 +97,7 @@ namespace GSMA.MobileConnect.Test
 
             Assert.AreEqual(MobileConnectResponseType.Complete, status.ResponseType);
             Assert.IsNotEmpty(status.TokenResponse.ResponseData.AccessToken);
+            Assert.IsNotEmpty(status.TokenResponse.ResponseData.IdToken);
         }
 
         [TestCase("SandboxR2")]
@@ -137,6 +139,51 @@ namespace GSMA.MobileConnect.Test
 
             Assert.AreEqual(MobileConnectResponseType.Complete, status.ResponseType);
             Assert.IsNotEmpty(status.TokenResponse.ResponseData.AccessToken);
+            Assert.IsNotEmpty(status.TokenResponse.ResponseData.IdToken);
+        }
+
+        [TestCase("SandboxR2")]
+        public async Task MobileConnectWebInterfaceShouldWorkEndToEndHeadlessWithAutoIdentity(string configKey)
+        {
+            RestClient restClient = new RestClient();
+            ICache cache = null;
+            IDiscoveryService discovery = new GSMA.MobileConnect.Discovery.DiscoveryService(cache, restClient);
+            IAuthenticationService authentication = new GSMA.MobileConnect.Authentication.AuthenticationService(restClient);
+            IIdentityService identity = new GSMA.MobileConnect.Identity.IdentityService(restClient);
+            IJWKeysetService jwks = new GSMA.MobileConnect.Authentication.JWKeysetService(restClient, cache);
+
+            var testConfig = TestConfig.GetConfig(configKey);
+            MobileConnectConfig config = new MobileConnectConfig()
+            {
+                DiscoveryUrl = testConfig.DiscoveryUrl,
+                ClientId = testConfig.ClientId,
+                ClientSecret = testConfig.ClientSecret,
+                RedirectUrl = testConfig.RedirectUrl
+            };
+
+            MobileConnectRequestOptions blankOptions = new MobileConnectRequestOptions();
+            MobileConnectWebInterface mobileConnect = new MobileConnectWebInterface(discovery, authentication, identity, jwks, config);
+
+            //Attempt discovery
+            var request = new HttpRequestMessage();
+            var status = await mobileConnect.AttemptDiscoveryAsync(request, testConfig.ValidMSISDN, null, null, true, blankOptions);
+
+            Assert.AreEqual(MobileConnectResponseType.StartAuthentication, status.ResponseType);
+
+            var discoveryResponse = status.DiscoveryResponse;
+            var encryptedMsisdn = status.DiscoveryResponse.ResponseData.subscriber_id;
+            var state = "zmxncbvalskdjfhgqpwoeiruty";
+            var nonce = "qpwoeirutyalskdjfhgzmxncbv";
+
+            //Start Authorization
+            var authOptions = new MobileConnectRequestOptions { Scope = "mc_authz mc_identity_phonenumber mc_identity_signup", BindingMessage = "auth test", Context = "test", AutoRetrieveIdentityHeadless = true };
+            request = new HttpRequestMessage();
+            status = await mobileConnect.RequestHeadlessAuthenticationAsync(request, discoveryResponse, encryptedMsisdn, state, nonce, authOptions);
+
+            Assert.AreEqual(MobileConnectResponseType.Complete, status.ResponseType);
+            Assert.IsNotEmpty(status.TokenResponse.ResponseData.AccessToken);
+            Assert.IsNotNull(status.IdentityResponse);
+            Assert.IsNotEmpty(status.IdentityResponse.ResponseJson);
         }
     }
 }
