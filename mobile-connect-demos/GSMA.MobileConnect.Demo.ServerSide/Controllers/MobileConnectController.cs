cdeb852bea7bcf5f2ec09c1812fecd6213529252
DmitryMaretskyA1QA <mareckiyinfo@yandex.ru>
2018-01-23 23:49:07 +0300

    reworked server side demo application

--- mobile-connect-demos/GSMA.MobileConnect.Demo.ServerSide/Controllers/MobileConnectController.cs
+++ mobile-connect-demos/GSMA.MobileConnect.Demo.ServerSide/Controllers/MobileConnectController.cs
@@ -12,6 +12,7 @@ using System.Web.Http.Results;
 using GSMA.MobileConnect.Constants;
 using GSMA.MobileConnect.ServerSide.Web.Objects;
 using GSMA.MobileConnect.ServerSide.Web.Utils;
+using Newtonsoft.Json;
 
 namespace GSMA.MobileConnect.ServerSide.Web.Controllers
 {
@@ -44,25 +45,100 @@ namespace GSMA.MobileConnect.ServerSide.Web.Controllers
 
         [HttpGet]
         [Route("start_discovery")]
-        public async Task<IHttpActionResult> StartDiscovery(string msisdn = "", string mcc = "", string mnc = "", string sourceIp = "")
+        public async Task<string> StartDiscovery(string msisdn = "", string mcc = "", string mnc = "", string sourceIp = "")
         {
             GetParameters();
             var requestOptions = new MobileConnectRequestOptions { ClientIP = sourceIp };
             var status = await _mobileConnect.AttemptDiscoveryAsync(Request, msisdn, mcc, mnc, true, _includeRequestIP, requestOptions);
-
             CachedParameters.sdkSession = status.SDKSession;
-             
-            return CreateResponse(status);
+            var authResponse = await StartAuthentication(status.SDKSession, status.DiscoveryResponse.ResponseData.subscriber_id);
+           
+            return authResponse;
         }
 
         [HttpGet]
-        [Route("start_authentication"), Route("start_authorization")]
+        [Route("headless_authentication")]
+        public async Task<IHttpActionResult> RequestHeadlessAuthentication(string sdksession = null, string subscriberId = null, string scope = null)
+        {
+            var options = new MobileConnectRequestOptions
+            {
+                Scope = scope,
+                Context = "headless",
+                BindingMessage = "demo headless",
+                AutoRetrieveIdentityHeadless = true,
+            };
+
+            var response = await _mobileConnect.RequestHeadlessAuthenticationAsync(Request, sdksession, subscriberId, null, null, options);
+            return CreateResponse(response);
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> HandleRedirect(string sdksession = null, string mcc_mnc = null, string code = null, string expectedState = null, string expectedNonce = null)
+        {
+            // Accept valid results and results indicating validation was skipped due to missing support on the provider
+            var requestOptions = new MobileConnectRequestOptions { AcceptedValidationResults = Authentication.TokenValidationResult.Valid | Authentication.TokenValidationResult.IdTokenValidationSkipped };
+            var response = await _mobileConnect.HandleUrlRedirectAsync(Request, Request.RequestUri, sdksession, expectedState, expectedNonce, requestOptions);
+
+            return CreateResponse(response);
+        }
+
+        [HttpGet]
+        [Route("discovery_callback")]
+        public async Task<IHttpActionResult> DiscoveryCallback(string state)
+        {
+            var requestOptions = new MobileConnectRequestOptions { AcceptedValidationResults = Authentication.TokenValidationResult.Valid | Authentication.TokenValidationResult.IdTokenValidationSkipped };
+            var cachedInfo = responseChecker.getData(state);
+            var authConnectStatus = await _mobileConnect.HandleUrlRedirectAsync(Request, Request.RequestUri, cachedInfo.Result.sdkSession, state, cachedInfo.Result.nonce, requestOptions);
+            MobileConnectStatus response = null;
+            var idTokenResponseModel = JsonConvert.DeserializeObject<IdTokenResponse>(authConnectStatus.TokenResponse.DecodedIdTokenPayload);
+            if (idTokenResponseModel.nonce.Equals(cachedInfo.Result.nonce))
+            {
+                if (_operatorParams.identity == "True")
+                {
+                    response = await RequestUserInfo(state, authConnectStatus.TokenResponse.ResponseData.AccessToken);
+                    return CreateIdentityResponse(response, authConnectStatus);
+                }
+                else if (_operatorParams.userInfo == "True")
+                {
+                    response = await RequestIdentity(state, authConnectStatus.TokenResponse.ResponseData.AccessToken);
+                    return CreateIdentityResponse(response, authConnectStatus);
+                }
+            }
+            else
+            {
+                response = MobileConnectStatus.Error(ErrorCodes.InvalidArgument, "nonce is incorrect", new Exception());
+            }
+            return CreateResponse(response);
+        }
+
+        public async Task<MobileConnectStatus> RequestUserInfo(string state = null, string accessToken = null)
+        {
+            var cachedInfo = responseChecker.getData(state);
+            MobileConnectStatus response = null;
+            try
+            {
+                response = await _mobileConnect.RequestUserInfoAsync(Request, cachedInfo.Result.sdkSession,
+                    accessToken == null ? cachedInfo.Result.accessToken : accessToken, new MobileConnectRequestOptions());
+            }
+            catch (Exception e)
+            {
+                if (cachedInfo.Result != null)
+                {
+                    return response;
+                }
+                response = MobileConnectStatus.Error(ErrorCodes.InvalidArgument, "state value is incorrect", e);
+            }
+           
+            return response;
+        }
+
         public async Task<string> StartAuthentication(string sdksession = null, string subscriberId = null)
         {
             string scope = _operatorParams.scope;
             if (scope == null && _operatorUrLs.ProviderMetadataUrl == null)
                 _apiVersion = Utils.Constants.Version1;
-            else if (scope==null)
+            else if (scope == null)
                 _apiVersion = Utils.Constants.Version2;
 
             var options = new MobileConnectRequestOptions
@@ -83,48 +159,7 @@ namespace GSMA.MobileConnect.ServerSide.Web.Controllers
             return response.Url;
         }
 
-        [HttpGet]
-        [Route("headless_authentication")]
-        public async Task<IHttpActionResult> RequestHeadlessAuthentication(string sdksession = null, string subscriberId = null, string scope = null)
-        {
-            var options = new MobileConnectRequestOptions
-            {
-                Scope = scope,
-                Context = "headless",
-                BindingMessage = "demo headless",
-                AutoRetrieveIdentityHeadless = true,
-            };
-
-            var response = await _mobileConnect.RequestHeadlessAuthenticationAsync(Request, sdksession, subscriberId, null, null, options);
-            return CreateResponse(response);
-        }
-
-        [HttpGet]
-        [Route("user_info")]
-        public async Task<IHttpActionResult> RequestUserInfo(string state = null, string accessToken = null)
-        {
-            var cachedInfo = responseChecker.getData(state);
-            MobileConnectStatus response = null;
-            try
-            {
-                response = await _mobileConnect.RequestUserInfoAsync(Request, cachedInfo.Result.sdkSession,
-                    accessToken == null ? cachedInfo.Result.accessToken : accessToken, new MobileConnectRequestOptions());
-            }
-            catch (Exception e)
-            {
-                if (cachedInfo.Result != null)
-                {
-                    return CreateResponse(response);
-                }
-                response = MobileConnectStatus.Error(ErrorCodes.InvalidArgument, "state value is incorrect", e);
-            }
-           
-            return CreateResponse(response);
-        }
-
-        [HttpGet]
-        [Route("identity")]
-        public async Task<IHttpActionResult> RequestIdentity(string state = null, string accessToken = null)
+        public async Task<MobileConnectStatus> RequestIdentity(string state = null, string accessToken = null)
         {
             var cachedInfo = responseChecker.getData(state);
             MobileConnectStatus response = null;
@@ -138,71 +173,17 @@ namespace GSMA.MobileConnect.ServerSide.Web.Controllers
             {
                 if (cachedInfo.Result != null)
                 {
-                    return CreateResponse(response);
+                    return response;
                 }
                 response = MobileConnectStatus.Error(ErrorCodes.InvalidArgument, "state value is incorrect", e);
             }
-            
-            return CreateResponse(response);
-        }
 
-        [HttpGet]
-        [Route("refresh_token")]
-        public async Task<IHttpActionResult> RefreshToken(string sdksession = null, string refreshToken = null)
-        {
-            var response = await _mobileConnect.RefreshTokenAsync(Request, refreshToken, sdksession);
-            return CreateResponse(response);
-        }
-
-        [HttpGet]
-        [Route("revoke_token")]
-        public async Task<IHttpActionResult> RevokeToken(string state = null, string accessToken = null)
-        {
-            var cachedInfo = responseChecker.getData(state);
-            MobileConnectStatus response = null;
-            try
-            {
-                response = await _mobileConnect.RequestIdentityAsync(Request, cachedInfo.Result.sdkSession,
-                    accessToken == null ? cachedInfo.Result.accessToken : accessToken, new MobileConnectRequestOptions());
-            }
-            catch (Exception e)
-            {
-                if (cachedInfo.Result != null)
-                {
-                    return CreateResponse(response);
-                }
-                response = MobileConnectStatus.Error(ErrorCodes.InvalidArgument, "state value is incorrect", e);
-            }
-            return CreateResponse(response);
-        }
-
-        [HttpGet]
-        [Route("")]
-        public async Task<IHttpActionResult> HandleRedirect(string sdksession=null, string mcc_mnc=null, string code=null, string expectedState=null, string expectedNonce=null)
-        {
-            // Accept valid results and results indicating validation was skipped due to missing support on the provider
-            var requestOptions = new MobileConnectRequestOptions { AcceptedValidationResults = Authentication.TokenValidationResult.Valid | Authentication.TokenValidationResult.IdTokenValidationSkipped };
-            var response = await _mobileConnect.HandleUrlRedirectAsync(Request, Request.RequestUri, sdksession, expectedState, expectedNonce, requestOptions);
-            
-            return CreateResponse(response);
-        }
-
-        [HttpGet]
-        [Route("discovery_callback")]
-        public async Task<IHttpActionResult> DiscoveryCallback(string state)
-        {
-            var requestOptions = new MobileConnectRequestOptions { AcceptedValidationResults = Authentication.TokenValidationResult.Valid | Authentication.TokenValidationResult.IdTokenValidationSkipped };
-            var cachedInfo = responseChecker.getData(state);
-            var response = await _mobileConnect.HandleUrlRedirectAsync(Request, Request.RequestUri, cachedInfo.Result.sdkSession, state, cachedInfo.Result.nonce, requestOptions);
-            CachedParameters.accessToken = response.TokenResponse.ResponseData.AccessToken;
-            await responseChecker.SaveData(state, CachedParameters);
-            return CreateResponse(response);
+            return response;
         }
 
         private IHttpActionResult CreateResponse(MobileConnectStatus status)
         {
             var response = Request.CreateResponse(HttpStatusCode.OK, ResponseConverter.Convert(status));
-
             if (status.SetCookie != null)
             {
                 foreach (var cookie in status.SetCookie)
@@ -210,8 +191,32 @@ namespace GSMA.MobileConnect.ServerSide.Web.Controllers
                     response.Headers.Add("Set-Cookie", cookie);
                 }
             }
-
             return new ResponseMessageResult(response);
+        }
+
+        private IHttpActionResult CreateIdentityResponse(MobileConnectStatus status, MobileConnectStatus authnStatus)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK, ResponseConverter.Convert(status));
+            var authnResponse = Request.CreateResponse(HttpStatusCode.OK, ResponseConverter.Convert(authnStatus));
+            authnResponse.Content = new StringContent(createNewHttpResponseMessage(response, authnResponse));
+            if (status.SetCookie != null)
+            {
+                foreach (var cookie in status.SetCookie)
+                {
+                    authnResponse.Headers.Add("Set-Cookie", cookie);
+                }
+            }
+            return new ResponseMessageResult(authnResponse);
+        }
+
+        private string createNewHttpResponseMessage(HttpResponseMessage response, HttpResponseMessage authnResponse)
+        {
+            dynamic convertResponseToJson = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result);
+            dynamic convertAuthnResponseToJson = JsonConvert.DeserializeObject(authnResponse.Content.ReadAsStringAsync().Result);
+            return "{" + "\"access_token\":" + "\"" + convertAuthnResponseToJson.token.access_token + "\"" + 
+                ", \"token_type\":" + "\"" + convertAuthnResponseToJson.token.token_type + "\"" + 
+                ", \"id_token\":" + "\"" + convertAuthnResponseToJson.token.id_token + "\"" + 
+                ", \"identity\":" + convertResponseToJson.identity + "}";
         }
 
         private void GetParameters()
