2ee14aa61f4c4709863eee025487de8acbe14b03
Titova, Polina <p.titova@a1qa.com>
2018-11-08 14:48:38 +0300

    Add Scopes

--- mobile-connect-sdk/GSMA.MobileConnect/Constants/Scope.cs
+++ mobile-connect-sdk/GSMA.MobileConnect/Constants/Scope.cs
@@ -6,10 +6,23 @@
         public const string AUTHN = "mc_authn";
         public const string AUTHZ = "mc_authz";
 
+        public const string PROFILE = "profile";
+        public const string EMAIL = "email";
+        public const string ADDRESS = "address";
+        public const string PHONE = "phone";
+        public const string OFFLINEACCESS = "offline_access";
+
+        public const string IDENTITY = "mc_identity";
         public const string IDENTITYPHONE = "mc_identity_phonenumber";
         public const string IDENTITYSIGNUP = "mc_identity_signup";
         public const string IDENTITYSIGNUPPLUS = "mc_identity_signupplus";
         public const string IDENTITYNATIONALID = "mc_identity_nationalid";
+
+        public const string KYCPLAIN = "mc_kyc_plain";
+        public const string KYCHASHED = "mc_kyc_hashed";
+        public const string ATTRVMSHARE = "mc_attr_vm_share";
+        public const string ATTRVMMATCH = "mc_attr_vm_match";
+        public const string ATTRVMMATCHHASH = "mc_attr_vm_match_hash";
 
         public const string MCPREFIX = "mc_";
     }
