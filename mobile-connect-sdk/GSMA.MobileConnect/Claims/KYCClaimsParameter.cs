72b594faca129c6383d9aac52d2a3d28bf97650c
Titova, Polina <p.titova@a1qa.com>
2018-11-12 15:29:21 +0300

    Add KYC Claims

--- mobile-connect-sdk/GSMA.MobileConnect/Claims/KYCClaimsParameter.cs
+++ mobile-connect-sdk/GSMA.MobileConnect/Claims/KYCClaimsParameter.cs
@@ -65,5 +65,59 @@ namespace GSMA.MobileConnect.Claims
         /// </summary>
         [JsonProperty("birthdate", NullValueHandling = NullValueHandling.Ignore)]
         public string Birthdate { get; set; }
+
+        /// <summary>
+        /// Concatenated given_name and family_name. 
+        /// </summary>
+        [JsonProperty("name_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string NameHashed { get; set; }
+
+        /// <summary>
+        /// Given name(s) or first name(s) of the End-User.
+        /// </summary>
+        [JsonProperty("given_name_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string GivenNameHashed { get; set; }
+
+        /// <summary>
+        /// Family name(s), surname(s) or last name(s) of the End-User.
+        /// </summary>
+        [JsonProperty("family_name_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string FamilyNameHashed { get; set; }
+
+        /// <summary>
+        /// Concatenated houseno_or_housename, postal_code and optionally town and country. 
+        /// </summary>
+        [JsonProperty("address_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string AddressHashed { get; set; }
+
+        /// <summary>
+        /// Registered house number or house name.
+        /// </summary>
+        [JsonProperty("houseno_or_housename_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string HousenoOrHouseNameHashed { get; set; }
+
+        /// <summary>
+        /// Registered Zip code or post code.
+        /// </summary>
+        [JsonProperty("postal_code_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string PostalCodeHashed { get; set; }
+
+        /// <summary>
+        /// Registered city or town name.
+        /// </summary>
+        [JsonProperty("town_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string TownHashed { get; set; }
+
+        /// <summary>
+        /// Registered country.
+        /// </summary>
+        [JsonProperty("country_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string CountryHashed { get; set; }
+
+        /// <summary>
+        /// End-User's birthday.
+        /// </summary>
+        [JsonProperty("birthdate_hashed", NullValueHandling = NullValueHandling.Ignore)]
+        public string BirthdateHashed { get; set; }
     }
 }
